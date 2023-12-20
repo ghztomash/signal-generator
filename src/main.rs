55f82ecf8dc2b1031d65320e98d3551703b59eb8
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-21 00:01:32 +0100

    Main function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-use color_eyre::eyre::Result;
-
 mod app;
 mod event;
 mod tui;
 mod ui;
 mod update;
 
+use app::App;
+use color_eyre::eyre::Result;
+use event::{Event, EventHandler};
+use ratatui::{backend::CrosstermBackend, Terminal};
+use tui::Tui;
+use update::update;
+
 fn main() -> Result<()> {
     color_eyre::install()?;
-    println!("Hello, world!");
+    // Create apllication instance
+    let mut app = App::new();
 
+    // Initialize terminal
+    let backend = CrosstermBackend::new(std::io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.enter()?;
+
+    // Start the main loop
+    while !app.should_quit {
+        // Render the UI
+        tui.draw(&mut app)?;
+
+        // Handle events
+        match tui.event_handler.next()? {
+            Event::Tick => {}
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        };
+    }
+
+    // Exit the terminal
+    tui.exit()?;
     Ok(())
 }
