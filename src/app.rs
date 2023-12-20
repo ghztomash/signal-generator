732153e914197c03451dbdec42eb410e6348255c
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-20 22:22:57 +0100

    app state

---
+++ src/app.rs
@@ -0,0 +1,31 @@
+/// Application state
+
+#[derive(Debug, Default)]
+pub struct App {
+    pub should_quit: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn tick(&self) {}
+
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_app_quit() {
+        let mut app = App::new();
+        assert_eq!(app.should_quit, false);
+        app.quit();
+        assert_eq!(app.should_quit, true);
+    }
+}
