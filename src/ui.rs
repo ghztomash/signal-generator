8b8b94a7dc444a9e29f81b0b98e4e8662e19c0e0
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-22 02:47:21 +0100

    Re arranged

--- src/ui.rs
+++ src/ui.rs
@@ -13,15 +13,7 @@ use crate::app::App;
 pub fn render(app: &mut App, frame: &mut Frame) {
     frame.render_widget(Clear, frame.size());
 
-    let block = Block::default()
-        .title("Signal Generator")
-        .title_alignment(Alignment::Center)
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded);
-
     let area = frame.size();
-
-    frame.render_widget(block, area);
 
     let sub_area = Layout::default()
         .direction(Direction::Vertical)
@@ -30,53 +22,46 @@ pub fn render(app: &mut App, frame: &mut Frame) {
             Constraint::Min(1),
             Constraint::Length(3),
         ])
-        .split(area.inner(&Margin {
-            horizontal: 1,
-            vertical: 1,
-        }));
+        .split(area);
 
-    frame.render_widget(make_tab_bar(app), sub_area[0]);
+    let title_area = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(sub_area[0]);
 
+    // main area is split into two, left preview and right control panel
     let main_sub_area = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .split(sub_area[1].inner(&Margin {
+        .split(sub_area[1]);
+
+    let tab_area = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(3), Constraint::Min(1)])
+        .split(main_sub_area[1].inner(&Margin {
             horizontal: 1,
             vertical: 1,
         }));
 
-    frame.render_widget(
-        Block::default()
-            .title("Sub-block")
-            .title_alignment(Alignment::Left)
-            .borders(Borders::ALL)
-            .border_type(BorderType::Rounded),
-        sub_area[1],
-    );
+    frame.render_widget(make_tab_bar(app), title_area[1]);
 
     frame.render_widget(make_preview_canvas(), main_sub_area[0]);
 
-    frame.render_widget(make_chart(), main_sub_area[1]);
-
     frame.render_widget(
-        Paragraph::new(format!("Press `Esc`, `Ctrl-C` or `q` to stop running."))
-            .block(
-                Block::default()
-                    .title("Status")
-                    .title_alignment(Alignment::Left)
-                    .title_style(Style::default().fg(Color::Green))
-                    .borders(Borders::ALL)
-                    .border_type(BorderType::Rounded),
-            )
-            .style(Style::default().fg(Color::Yellow))
-            .alignment(Alignment::Left),
-        sub_area[2],
-    )
+        Block::default()
+            .title("Control Panel")
+            .title_alignment(Alignment::Left)
+            .borders(Borders::ALL)
+            .border_type(BorderType::Rounded),
+        main_sub_area[1],
+    );
+
+    frame.render_widget(make_status_bar(), sub_area[2])
 }
 
-fn make_tab_bar(app: &mut App) -> Tabs<'static> {
+fn make_tab_bar(app: &mut App) -> impl Widget + 'static {
     let tab_titles = vec!["Tab1", "Tab2", "Tab3", "Tab4"];
-    let tabs = Tabs::new(tab_titles)
+    Tabs::new(tab_titles)
         .block(
             Block::default()
                 .title("Tabs")
@@ -85,8 +70,7 @@ fn make_tab_bar(app: &mut App) -> Tabs<'static> {
         )
         .style(Style::default())
         .highlight_style(Style::default().fg(Color::Green))
-        .select(app.tab_index);
-    tabs
+        .select(app.tab_index)
 }
 
 fn make_preview_canvas() -> impl Widget + 'static {
@@ -109,36 +93,16 @@ fn make_preview_canvas() -> impl Widget + 'static {
         .y_bounds([-90.0, 90.0])
 }
 
-fn make_chart() -> Chart<'static> {
-    // Create the datasets to fill the chart with
-    let datasets = vec![
-        // Scatter chart
-        Dataset::default()
-            .name("data1")
-            .marker(Marker::Dot)
-            .graph_type(GraphType::Line)
-            .style(Style::default().cyan())
-            .data(&[(0.0, 5.0), (1.0, 6.0), (1.5, 6.434)]),
-        // Line chart
-        Dataset::default()
-            .name("data2")
-            .marker(Marker::Braille)
-            .graph_type(GraphType::Line)
-            .style(Style::default().magenta())
-            .data(&[(4.0, 5.0), (5.0, 8.0), (7.66, 13.5)]),
-    ];
-
-    // Create the X axis and define its properties
-    let x_axis = Axis::default().bounds([0.0, 10.0]);
-
-    // Create the Y axis and define its properties
-    let y_axis = Axis::default().bounds([0.0, 10.0]);
-
-    // Create the chart and link all the parts together
-    let chart = Chart::new(datasets)
-        .block(Block::default().title("Chart"))
-        .x_axis(x_axis)
-        .y_axis(y_axis);
-
-    chart
+fn make_status_bar() -> impl Widget + 'static {
+    Paragraph::new(format!("Press `Esc`, `Ctrl-C` or `q` to stop running."))
+        .block(
+            Block::default()
+                .title("Status")
+                .title_alignment(Alignment::Left)
+                .title_style(Style::default().fg(Color::Green))
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded),
+        )
+        .style(Style::default().fg(Color::Yellow))
+        .alignment(Alignment::Left)
 }
