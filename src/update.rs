3dd6491ee2ac73373e1888ac1237881f3158a1f4
Tomash Ghz <ghz.tomash@hotmail.com>
2024-01-30 02:49:39 +0100

    Command mode states

--- src/update.rs
+++ src/update.rs
@@ -1,21 +1,42 @@
-use crate::app::App;
+use crate::app::{App, Mode};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 pub fn update(app: &mut App, key_event: KeyEvent) {
+    if key_event.code == KeyCode::Esc {
+        app.set_normal_mode();
+        return;
+    } else if app.mode == Mode::Command {
+        // app.push_command_char(key_event.code);
+    }
     match key_event.code {
-        KeyCode::Esc | KeyCode::Char('q') => app.quit(),
+        KeyCode::Esc => {
+            app.set_normal_mode();
+        }
+        KeyCode::Char('q') => {
+            if app.mode == Mode::Normal {
+                app.quit();
+            }
+        }
         KeyCode::Char('c') | KeyCode::Char('C') => {
             if key_event.modifiers == KeyModifiers::CONTROL {
                 app.quit();
             }
         }
+        KeyCode::Char('h') | KeyCode::Char('H') => {
+            app.set_help_mode();
+        }
         KeyCode::Tab => app.next_tab(),
         KeyCode::BackTab => app.previous_tab(),
         KeyCode::Enter => {
-            app.set_command_mode(false);
             app.process_command();
+            app.set_normal_mode();
         }
-        KeyCode::Char(':') => app.set_command_mode(true),
+        KeyCode::Backspace => {
+            // if app.command_mode() {
+            //     app.pop_command_char();
+            // }
+        }
+        KeyCode::Char(':') => app.set_command_mode(),
         KeyCode::Char('1') => app.set_tab(0),
         KeyCode::Char('2') => app.set_tab(1),
         KeyCode::Char('3') => app.set_tab(2),
